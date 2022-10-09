7d70de189a2489a4eb28031bf48709124e062217
melony <hjz1010@gmail.com>
2022-10-09 10:19:24 +0900

    [ADD] Swagger 추가

--- app.js
+++ app.js
@@ -10,6 +10,10 @@ const createApp = () => {
   app.use(router);
   app.use(morgan("combined"));
   app.use(express.static("public"));
+
+  //swagger 연결
+  const { swaggerUi, specs } = require("./apidocs/swagger")
+  app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs))
   
   app.get("/ping", function (req, res, next) {
     res.json({ message: "pong" });
