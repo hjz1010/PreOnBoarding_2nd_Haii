f477b68e6130cacd9ea5c7af8abeeddb288a1b6a
Mark Kim <mark340@naver.com>
2022-10-09 22:49:13 +0900

    [ADD] users/signUp swagger API Docs AND code refactoring

--- services/user.js
+++ services/user.js
@@ -11,18 +11,27 @@ const signUp = async (account, password, name, phone_number, type_id, region_id)
   if (!account || !password || !name || !phone_number || !type_id) {
     throw new BaseError("입력된 값이 없는 항목이 있습니다.", 400);
   }
-  validationEmail(account); // 이메일형식 유효성 검사
-  validationPassword(password); // 비밀번호형식 유효성 검사
-  validationName(name); // 이름 유효성 검사
-  validationPhone(phone_number); // 휴대폰번호 유효성 검사
+  validationEmail(account);
+  validationPassword(password);
+  validationName(name);
+  validationPhone(phone_number);
 
-  const isExistAccount = await userDao.existCheckUserByAccount(account); // 가입여부 확인
+  const isExistAccount = await userDao.existCheckUserByAccount(account);
   if (isExistAccount) {
-    throw new Error("이미 존재하는 계정입니다", 400);
+    throw new BaseError("이미 존재하는 계정입니다", 400);
   }
 
   if (type_id === 1 && region_id) {
-    throw new Error("담당 지역 형식이 올바르지 않습니다.", 400); // type_id가 1인 경우 - 대표관리자 (담당지역없음)
+    throw new BaseError("담당 지역 형식이 올바르지 않습니다.", 400);
+  }
+
+  if (type_id > 2) {
+    throw new BaseError("존재하지 않는 유저타입 입니다.", 404);
+  }
+
+  const checkExistRegionId = await userDao.getRegionById(region_id);
+  if (!checkExistRegionId) {
+    throw new BaseError("존재하지 않는 지역ID 입니다.", 404);
   }
 
   const hashedPassword = await bcrypt.hash(password, 12);
@@ -30,7 +39,6 @@ const signUp = async (account, password, name, phone_number, type_id, region_id)
 };
 
 const login = async (account, password) => {
-
   const user = await userDao.getUserByAccount(account);
   if (!user) {
     const error = new Error("이메일과 비밀번호를 확인해주세요.");
@@ -46,13 +54,13 @@ const login = async (account, password) => {
   }
   // 토큰 완료시간 1시간으로 설정
   const accessToken = jwt.sign({ user_id: user.id, type_id: user.type_id }, SECRET_KEY, {
-    expiresIn: "1h", 
-    issuer: ISSUER 
+    expiresIn: "1h",
+    issuer: ISSUER,
   });
   // 리프레시 토큰은 만료시간 14일
-  const refreshToken = jwt.sign({}, SECRET_KEY, { 
-    expiresIn: "14d", 
-    issuer: ISSUER 
+  const refreshToken = jwt.sign({}, SECRET_KEY, {
+    expiresIn: "14d",
+    issuer: ISSUER,
   });
 
   await userDao.createRefreshToken(refreshToken, user.id);
@@ -62,51 +70,51 @@ const login = async (account, password) => {
 };
 
 const checkUserExist = async (user_id) => {
-  const user = await userDao.getUserByUserId(user_id)
+  const user = await userDao.getUserByUserId(user_id);
   if (!user) {
     throw new BaseError("해당 유저는 존재하지 않습니다.", 404);
   }
-}
+};
 
 const checkUserTypeId = async (user_type_id) => {
   if (user_type_id === 2) {
     throw new BaseError("지역 담당자는 정보 수정 권한이 없습니다.", 403);
   }
-}
+};
 
 const updateName = async (user_id, name) => {
-  validationName(name)
-  const savedData = await userDao.getUserNameByIdAndName(user_id, name)
+  validationName(name);
+  const savedData = await userDao.getUserNameByIdAndName(user_id, name);
   if (!savedData) {
-    await userDao.updateName(user_id, name)
+    await userDao.updateName(user_id, name);
   } else {
     throw new BaseError("기존 이름과 동일합니다.", 400);
   }
-}
+};
 
 const updatePhone = async (user_id, phone) => {
-  validationPhone(phone)
-  const savedData = await userDao.getUserPhoneByIdAndPhone(user_id, phone)
+  validationPhone(phone);
+  const savedData = await userDao.getUserPhoneByIdAndPhone(user_id, phone);
   if (!savedData) {
-    await userDao.updatePhone(user_id, phone)
+    await userDao.updatePhone(user_id, phone);
   } else {
     throw new BaseError("기존 연락처와 동일합니다.", 400);
   }
-}
+};
 
 const updateRegion = async (user_id, region_id) => {
-  const region = await userDao.getRegionById(region_id)
+  const region = await userDao.getRegionById(region_id);
   if (!region) {
     throw new BaseError("존재하지 않는 지역ID 입니다.", 404);
   }
 
-  const savedData = await userDao.getUserRegionById(user_id, region_id)
+  const savedData = await userDao.getUserRegionById(user_id, region_id);
   if (!savedData) {
-    await userDao.updateRegion(user_id, region_id)
+    await userDao.updateRegion(user_id, region_id);
   } else {
     throw new BaseError("기존 지역ID와 동일합니다.", 404);
   }
-}
+};
 
 module.exports = {
   signUp,
@@ -115,5 +123,5 @@ module.exports = {
   checkUserTypeId,
   updateName,
   updatePhone,
-  updateRegion
-}
+  updateRegion,
+};
--- utils/validation.js
+++ utils/validation.js
@@ -1,36 +1,30 @@
+const { BaseError } = require("../middlewares/appError");
+
 const validationEmail = (email) => {
   const emailRegExp = new RegExp(/^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/);
   if (!emailRegExp.test(email)) {
-    const err = new Error("이메일 형식이 맞지 않습니다.");
-    err.statusCode = 400;
-    throw err;
+    throw new BaseError("이메일 형식이 맞지 않습니다.", 400);
   }
 };
 
 const validationPassword = (password) => {
   const pwRegExp = new RegExp(/^(?=.*[A-Za-z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{8,20}$)/);
   if (!pwRegExp.test(password)) {
-    const err = new Error("비밀번호 형식이 맞지 않습니다.");
-    err.statusCode = 400;
-    throw err;
+    throw new Error("비밀번호 형식이 맞지 않습니다.", 400);
   }
 };
 
 const validationName = (name) => {
   const nameRegExp = new RegExp(/^[가-힣]{2,4}|[a-zA-Z]{2,10}\s[a-zA-Z]{2,10}$/);
   if (!nameRegExp.test(name)) {
-    const err = new Error("이름 형식이 맞지 않습니다.");
-    err.statusCode = 400;
-    throw err;
+    throw new Error("이름 형식이 맞지 않습니다.");
   }
 };
 
 const validationPhone = (phoneNumber) => {
   const phoneRegExp = new RegExp(/01[016789]-[^0][0-9]{2,3}-[0-9]{3,4}/);
   if (!phoneRegExp.test(phoneNumber)) {
-    const err = new Error("휴대폰번호 형식이 맞지 않습니다.");
-    err.statusCode = 400;
-    throw err;
+    throw new Error("휴대폰번호 형식이 맞지 않습니다.", 400);
   }
 };
 
