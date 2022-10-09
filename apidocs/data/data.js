3c04e1e1871f6da4a117901d0cc7fe5e9a447d91
jeehooh <110149590+jeehooh@users.noreply.github.com>
2022-10-09 23:08:29 +0900

    [FIX] swagger 오류 수정

--- apidocs/data/data.js
+++ apidocs/data/data.js
@@ -3,11 +3,18 @@
  *
  * /data:
  *  post:
- *  security:
+ *    security:
  *      - Authorization: []
- *  summary: "데이터 입력 기능"
- *  description:
+ *    summary: "데이터 입력 기능"
+ *    description:
  *    tags: [data]
+ *    parameters:
+ *     - in: path
+ *       name: token
+ *       required: true
+ *       description: 권한 확인용 토큰
+ *       schema:
+ *        type: string
  *    requestheader:
  *      description: 회원의 권한 확인 후 데이터 입력
  *      required: true
@@ -18,7 +25,8 @@
  *            properties:
  *              token:
  *                type: string
- *                token: "token"
+ *                description: "토큰"
+ *                example: ["eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoxLCJpYXQiOjE2NjUxOTk0MDIsImV4cCI6MTY2NTIwMzAwMn0.oEZCttrmA9VIahwXMkDyX63W6PL_Q3hTE0PCAZs3o6g", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE2NjUxOTk0MDIsImV4cCI6MTY2NjQwOTAwMn0.aTvRrwSZDYRrwGBX-F6cGIZ3oz_uHFStJGXrwxUp3UU"]
  *    responses:
  *      400:
  *        description: 토큰 미지급
@@ -29,11 +37,11 @@
  *              properties:
  *                status:
  *                    type: number
- *                    example: 403
+ *                    example: 400
  *                message:
  *                    type: string
  *                    example: "유저 인증이 필요합니다."
- *      402:
+ *      401:
  *        description: 권한이 없는 회원
  *        content:
  *          application/json:
@@ -42,7 +50,7 @@
  *              properties:
  *                status:
  *                    type: number
- *                    example: 402
+ *                    example: 401
  *                message:
  *                    type: string
  *                    example: "인증에 실패하였습니다."
